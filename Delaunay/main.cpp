f93062bffb27ba1b39ef9c0381b124ce2da9fd67
juanchuletas <gamj@xanum.uam.mx>
2021-01-11 11:04:33 -0600

    Second

--- Delaunay/main.cpp
+++ Delaunay/main.cpp
@@ -6,16 +6,12 @@
 
 int main(){
 
-    double array = {};
+    double array[] = {0.0,0.0};
+    std::cout<<"Delaunay Triangulation Module.... \n";
 
 
     /* delaunay.setEdgeGrid();
     delaunay.triangulate(points); */
-
-
-
-
-
 
     return 0;
 }
